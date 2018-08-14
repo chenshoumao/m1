23dd1a63dc4fe7a52ff531a49154ddb0d9ff5881
chenshoumao <1223991507@qq.com>
2018-08-14 11:24:42 +0800

    121

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(123);
+	}
+	
+	public String sayHello(String username) {
+		return  "this eeis " + username + "  kkkk";
+	}
+}
