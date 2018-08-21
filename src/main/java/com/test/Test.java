e837ff6424a59fcf79c2aef151eaa73bcafad895
chenshoumao <1223991507@qq.com>
2018-08-21 22:47:39 +0800

    1212

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -6,6 +6,6 @@ public class Test {
 	}
 	
 	public String sayHello(String username) {
-		return  "this world has " + username + " 2018115下午";
+		return  "this world has " + username + " 2018115121212";
 	}
 }
